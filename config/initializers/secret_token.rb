f7c070910f374568d49b712f7f7966470a8cf6e3
kellimohr <kellimohr@gmail.com>
2013-01-28 19:33:01 -0800

    Generated Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veganbff::Application.config.secret_token = 'f37f0089eb5b1d87938901365db01219d72ec80729b70a9343e2d35636ddcb4fa24bb2f67781d62646e92d7e83e668730ca557c694ee4104936d012539e10a74'
