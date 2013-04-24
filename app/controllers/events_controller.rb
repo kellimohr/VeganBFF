7e1f7dc8e821a89a992c5b64b208b81708884cb7
kellimohr <kellimohr@gmail.com>
2013-04-23 20:18:22 -0700

    Set up New Event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,8 @@
-class EventsController < ApplicationController
+class EventsController < InheritedResources::Base
   def index
     @events = Event.all
     @events_by_date = @events.group_by(&:held_on)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
+
 end
