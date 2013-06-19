414bde18bf4708b1ddbc90867dfd09275c27cf90
kellimohr <kellimohr@gmail.com>
2013-06-18 17:05:44 -0700

    Fixed typo.

--- db/migrate/20130618233241_change_data_type_for_recipe_id_on_ingredients.rb
+++ db/migrate/20130618233241_change_data_type_for_recipe_id_on_ingredients.rb
@@ -2,7 +2,7 @@ class ChangeDataTypeForRecipeIdOnIngredients < ActiveRecord::Migration
   def self.up
     connection.execute(%q{alter table ingredients
     	                  alter column recipe_id
-    	                  type integer using cast(string as integer)
+    	                  type integer using cast(recipe_id as integer)
     	                 }
     	               )
   end
